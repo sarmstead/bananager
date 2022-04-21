4e333f67b7e3326bc8830c62c3b6523bf92fdfe2
Sunjay Armstead <connect@sunjayarmstead.local>
2022-04-20 21:09:21 -0400

    Configure postgres and migrate

--- api/db/migrations/20220421010737_init_postgres/migration.sql
+++ api/db/migrations/20220421010737_init_postgres/migration.sql
@@ -1,12 +1,14 @@
 -- CreateTable
 CREATE TABLE "User" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "email" TEXT NOT NULL,
     "name" TEXT,
     "hashedPassword" TEXT NOT NULL,
     "salt" TEXT NOT NULL,
     "resetToken" TEXT,
-    "resetTokenExpiresAt" DATETIME
+    "resetTokenExpiresAt" TIMESTAMP(3),
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
